bc1c69818688952b5791f1ab4d91218349c01957
caiwendi <wendi.cai@infaith.com.cn>
2019-03-12 10:44:41 +0800

    构建项目

--- xiaoan/ios/xiaoanTests/xiaoanTests.m
+++ xiaoan/ios/xiaoanTests/xiaoanTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface xiaoanTests : XCTestCase
+@interface XiaoAnTests : XCTestCase
 
 @end
 
-@implementation xiaoanTests
+@implementation XiaoAnTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
